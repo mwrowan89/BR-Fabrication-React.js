d9343ca3c8259c8984eb5180fef319a8be607196
mwrowan89 <mwrowan89@gmail.com>
2024-05-07 17:34:59 -0600

    Added homepage

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import HomePage from './HomePage';
+import HomePage1 from './HomePage1';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HomePage />
+    <HomePage1 />
   </React.StrictMode>
 );
 
