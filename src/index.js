7b73ef63bb236ac23a7a794b4004e9c50ec511ca
mwrowan89 <mwrowan89@gmail.com>
2024-05-08 12:07:16 -0600

    Updated home page

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nav />
+    <div className='Nav'>
+    <Nav /><br/>
+    </div>
     <App />
+    
     <HomePage />
   </React.StrictMode>
 );
