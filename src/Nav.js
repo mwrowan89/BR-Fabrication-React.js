e1c303846cf4bb6122c8b0ec954a2a4d87f9b32b
mwrowan89 <mwrowan89@gmail.com>
2024-05-08 11:50:02 -0600

    Added nav background

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Nav.css"
+import InstagramLogo from './assets/Screenshot (19).png';
+import FacebookLogo from './assets/facbook logo11.png';
+
 function Nav() {
   return (
     <div className="greetings">
     <h1> BR Fabrication</h1><br/>
-    <h2>Heirloom Quality Furniture for Commercial & Residential Spaces</h2> <br/>
+    <h3>Heirloom Quality Furniture for Commercial & Residential Spaces</h3> <br/>
       <div className="links">
-        <a href="https://www.instagram.com/fromcheftotable/" target="_blank" rel="noopener noreferrer"><img id="insta" src="./assets/Screenshot (19).png" width="33" height="33" alt="Instagram link"/></a> &nbsp; || &nbsp;
-        <a href="https://www.facebook.com/fromcheftotable/?locale=it_IT" target="_blank" rel="noopener noreferrer"><img src="./assets/facbook logo11.png" width="22" height="22" alt="FaceBook Link"/></a>
+        <a href="https://www.instagram.com/fromcheftotable/" target="_blank" rel="noopener noreferrer"><img id="insta" src={InstagramLogo} width="33" height="33" alt="Instagram link"/></a> &nbsp; || &nbsp;
+        <a href="https://www.facebook.com/fromcheftotable/?locale=it_IT" target="_blank" rel="noopener noreferrer"><img src={FacebookLogo} width="22" height="22" alt="FaceBook Link"/></a>
       </div>
   </div>
   );
