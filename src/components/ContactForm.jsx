aeb47823f4de0b17b05e3ccc17aa54eb701c3780
mwrowan89 <mwrowan89@gmail.com>
2024-07-05 12:07:55 -0600

    Added form to contact page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
 const ContactForm = () => {
+  const contact = {
+    first: "Your",
+    last: "Name",
+    avatar: "https://placekitten.com/g/200/200",
+    twitter: "your_handle",
+    notes: "Some notes",
+    favorite: true,
+  };
+
   return (
     <div>
       <form>
         <h1>Please provide information to get in touch with us!</h1>
+        <form>
+          <div className="contact-input">
+            <h3>
+              First Name: <input type="text" />
+              Last Name: <input type="text" />
+            </h3>
+          </div>
+        </form>
       </form>
     </div>
   );
