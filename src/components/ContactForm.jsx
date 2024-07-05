ca600fce6228bbb8d162c5e95520eeae971fb78f
mwrowan89 <mwrowan89@gmail.com>
2024-07-05 12:04:33 -0600

    Cleaned up conact page

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div>
+      <form>
+        <h1>Please provide information to get in touch with us!</h1>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,85 +1,78 @@
-import { Form } from "react-router-dom";
 import Nav from "../components/Nav";
+import ContactForm from "../components/ContactForm";
 
 export default function ContactUs() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
-
   return (
     <>
-      {" "}
       <Nav />
-      <div id="contact">
-        <div>
-          <img key={contact.avatar} src={contact.avatar || null} alt="" />
-        </div>
-
-        <div>
-          <h1>
-            {contact.first || contact.last ? (
-              <>
-                {contact.first} {contact.last}
-              </>
-            ) : (
-              <i>No Name</i>
-            )}{" "}
-            <Favorite contact={contact} />
-          </h1>
-
-          {contact.twitter && (
-            <p>
-              <a
-                target="_blank"
-                href={`https://twitter.com/${contact.twitter}`}
-                rel="noreferrer"
-              >
-                {contact.twitter}
-              </a>
-            </p>
-          )}
-
-          {contact.notes && <p>{contact.notes}</p>}
-
-          <div>
-            <Form action="edit">
-              <button type="submit">Edit</button>
-            </Form>
-            <Form
-              method="post"
-              action="destroy"
-              onSubmit={(event) => {
-                if (false("Please confirm you want to delete this record.")) {
-                  event.preventDefault();
-                }
-              }}
-            >
-              <button type="submit">Delete</button>
-            </Form>
-          </div>
-        </div>
-      </div>
+      <ContactForm />
     </>
   );
 }
 
-function Favorite({ contact }) {
-  // yes, this is a `let` for later
-  let favorite = contact.favorite;
-  return (
-    <Form method="post">
-      <button
-        name="favorite"
-        value={favorite ? "false" : "true"}
-        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
-      >
-        {favorite ? "★" : "☆"}
-      </button>
-    </Form>
-  );
-}
+// function Favorite({ contact }) {
+//   let favorite = contact.favorite;
+//   return (
+//     <Form method="post">
+//       <button
+//         name="favorite"
+//         value={favorite ? "false" : "true"}
+//         aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+//       >
+//         {favorite ? "★" : "☆"}
+//       </button>
+//     </Form>
+//   );
+// }
+
+// {" "}
+//       <Nav />
+//       <div id="contact">
+//         <div>
+//           <img key={contact.avatar} src={contact.avatar || null} alt="" />
+//         </div>
+
+//         <div>
+//           <h1>
+//             {contact.first || contact.last ? (
+//               <>
+//                 {contact.first} {contact.last}
+//               </>
+//             ) : (
+//               <i>No Name</i>
+//             )}{" "}
+//             <Favorite contact={contact} />
+//           </h1>
+
+//           {contact.twitter && (
+//             <p>
+//               <a
+//                 target="_blank"
+//                 href={`https://twitter.com/${contact.twitter}`}
+//                 rel="noreferrer"
+//               >
+//                 {contact.twitter}
+//               </a>
+//             </p>
+//           )}
+
+//           {contact.notes && <p>{contact.notes}</p>}
+
+//           <div>
+//             <Form action="edit">
+//               <button type="submit">Edit</button>
+//             </Form>
+//             <Form
+//               method="post"
+//               action="destroy"
+//               onSubmit={(event) => {
+//                 if (false("Please confirm you want to delete this record.")) {
+//                   event.preventDefault();
+//                 }
+//               }}
+//             >
+//               <button type="submit">Delete</button>
+//             </Form>
+//           </div>
+//         </div>
+//       </div>
