41ce3dc3fa2a85c2472714466f1466b0b18a6449
mwrowan89 <mwrowan89@gmail.com>
2024-07-06 17:38:39 -0600

    Moved header CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
 import React from "react";
-import "./Header.css"
-import InstagramLogo from '../assets/Screenshot (19).png';
-import FacebookLogo from '../assets/facbook logo11.png';
+import "../css/Header.css";
+import InstagramLogo from "../assets/Screenshot (19).png";
+import FacebookLogo from "../assets/facbook logo11.png";
 
 function Header() {
   return (
     <div className="greetings">
-    <h1> BR Fabrication</h1>
-    <h3>Heirloom Quality Furniture for Commercial & Residential Spaces</h3> <br/>
+      <h1> BR Fabrication</h1>
+      <h3>
+        Heirloom Quality Furniture for Commercial & Residential Spaces
+      </h3>{" "}
+      <br />
       <div className="links">
-        <a href="https://www.instagram.com/fromcheftotable/" target="_blank" rel="noopener noreferrer"><img id="insta" src={InstagramLogo} width="33" height="33" alt="Instagram link"/></a> &nbsp; || &nbsp;
-        <a href="https://www.facebook.com/fromcheftotable/?locale=it_IT" target="_blank" rel="noopener noreferrer"><img src={FacebookLogo} width="22" height="22" alt="FaceBook Link"/></a>
+        <a
+          href="https://www.instagram.com/fromcheftotable/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            id="insta"
+            src={InstagramLogo}
+            width="33"
+            height="33"
+            alt="Instagram link"
+          />
+        </a>{" "}
+        &nbsp; || &nbsp;
+        <a
+          href="https://www.facebook.com/fromcheftotable/?locale=it_IT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={FacebookLogo} width="22" height="22" alt="FaceBook Link" />
+        </a>
       </div>
-  </div>
+    </div>
   );
 }
 
 export default Header;
-
