b7f2553d9d1806e5d4dcd7256d8b57e5735611f3
mwrowan89 <mwrowan89@gmail.com>
2024-07-07 14:14:15 -0600

    Positioning and titles fixed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -22,47 +22,52 @@ const ContactForm = () => {
       <div className="contact-us-image">
         <img id="contact-us-image" src={image} alt="andros taverna" />
       </div>
-      <div className="contact-us-container">
-        <form className="contact-us-form">
-          <h1 id="contact-us-title">Contact Us</h1>
-          <label>
-            Full Name <br />
-            <input
-              id="textarea"
-              type="text"
-              name="full"
-              value={contact.first}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Email <br />
-            <input
-              id="textarea"
-              type="text"
-              name="email"
-              value={contact.email}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Message <br />
-            <textarea
-              id="textarea"
-              name="message"
-              value={contact.notes}
-              onChange={handleChange}
-            />
-          </label>
-          <button id="contact-us-button" type="submit">
-            Contact Us
-          </button>
-        </form>
-        <div className="our-info">
-          <h2>Contact</h2>
-          <p>ben@fromcheftotable.com</p>
+
+      <form className="contact-us-form">
+        <h1 id="contact-us-title">Contact Us</h1>
+        <div className="contact-us-container">
+          <div className="contact-us-labels">
+            <label>
+              Full Name <br />
+              <input
+                id="textarea"
+                type="text"
+                name="full"
+                value={contact.first}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Email <br />
+              <input
+                id="textarea"
+                type="text"
+                name="email"
+                value={contact.email}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Message <br />
+              <textarea
+                id="textarea"
+                name="message"
+                value={contact.notes}
+                onChange={handleChange}
+              />
+            </label>
+            <button id="contact-us-button" type="submit">
+              Contact Us
+            </button>
+          </div>
+          <div className="our-info">
+            <h2>Contact</h2>
+            <p>ben@fromcheftotable.com</p>
+            <h2>Based in</h2>
+            <p>Chicago, IL</p>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
