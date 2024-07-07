0bd5eaa9b5403dc4be6d6c401a27a0a34ad9b902
mwrowan89 <mwrowan89@gmail.com>
2024-07-06 18:09:53 -0600

    Form added values to variable contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,26 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/ContactForm.css";
 
 const ContactForm = () => {
-  const contact = {
+  const [contact, setContact] = useState({
     first: "Your",
     last: "Name",
     avatar: "https://placekitten.com/g/200/200",
     twitter: "your_handle",
     notes: "Some notes",
-    favorite: true,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContact({
+      ...contact,
+      [name]: value,
+    });
   };
 
   return (
     <div className="contact-us">
       <h1>Please provide information to get in touch with us!</h1>
-      <form className="contact-us-form">
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname" />
-        <label for="lname">Last name:</label>
-        <input type="text" id="lname" name="lname" />
-        <textarea type="text" id="query" />
-        <input type="submit" value="Submit" />
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="first"
+            value={contact.first}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="last"
+            value={contact.last}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Avatar URL:
+          <input
+            type="text"
+            name="avatar"
+            value={contact.avatar}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Twitter Handle:
+          <input
+            type="text"
+            name="twitter"
+            value={contact.twitter}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Notes:
+          <textarea
+            name="notes"
+            value={contact.notes}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
