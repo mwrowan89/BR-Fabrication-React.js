62d28b4008202a0b7bbb03f35cb93c2aafadc309
mwrowan89 <mwrowan89@gmail.com>
2024-07-08 13:48:56 -0600

    Added gallery img css

--- src/components/GalleryPhotos.js
+++ src/components/GalleryPhotos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/GalleryPhotos.css";
 import image1 from "../assets/images/1.jpg";
 import image2 from "../assets/images/2.jpg";
 import image3 from "../assets/images/9.jpeg";
@@ -24,29 +25,29 @@ import image24 from "../assets/images/16.png";
 
 const GalleryPhotos = () => {
   return (
-    <div>
-      <img src={image1} alt="image1" />
-      <img src={image2} alt="image1" />
-      <img src={image3} alt="image1" />
-      <img src={image4} alt="image1" />
-      <img src={image5} alt="image1" />
-      <img src={image6} alt="image1" />
-      <img src={image7} alt="image1" />
-      <img src={image8} alt="image1" />
-      <img src={image9} alt="image1" />
-      <img src={image10} alt="image1" />
-      <img src={image11} alt="image1" />
-      <img src={image12} alt="image1" />
-      <img src={image14} alt="image1" />
-      <img src={image15} alt="image1" />
-      <img src={image16} alt="image1" />
-      <img src={image17} alt="image1" />
-      <img src={image18} alt="image1" />
-      <img src={image20} alt="image1" />
-      <img src={image21} alt="image1" />
-      <img src={image22} alt="image1" />
-      <img src={image23} alt="image1" />
-      <img src={image24} alt="image1" />
+    <div className="gallery-images-conatiner">
+      <img id="gallery-img" src={image1} alt="image1" />
+      <img id="gallery-img" src={image2} alt="image1" />
+      <img id="gallery-img" src={image3} alt="image1" />
+      <img id="gallery-img" src={image4} alt="image1" />
+      <img id="gallery-img" src={image5} alt="image1" />
+      <img id="gallery-img" src={image6} alt="image1" />
+      <img id="gallery-img" src={image7} alt="image1" />
+      <img id="gallery-img" src={image8} alt="image1" />
+      <img id="gallery-img" src={image9} alt="image1" />
+      <img id="gallery-img" src={image10} alt="image1" />
+      <img id="gallery-img" src={image11} alt="image1" />
+      <img id="gallery-img" src={image12} alt="image1" />
+      <img id="gallery-img" src={image14} alt="image1" />
+      <img id="gallery-img" src={image15} alt="image1" />
+      <img id="gallery-img" src={image16} alt="image1" />
+      <img id="gallery-img" src={image17} alt="image1" />
+      <img id="gallery-img" src={image18} alt="image1" />
+      <img id="gallery-img" src={image20} alt="image1" />
+      <img id="gallery-img" src={image21} alt="image1" />
+      <img id="gallery-img" src={image22} alt="image1" />
+      <img id="gallery-img" src={image23} alt="image1" />
+      <img id="gallery-img" src={image24} alt="image1" />
     </div>
   );
 };
