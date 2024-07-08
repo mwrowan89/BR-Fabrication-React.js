7e98e3648b3ee72cd99e01326168d7511c2c0853
mwrowan89 <mwrowan89@gmail.com>
2024-07-08 15:50:27 -0600

    Updated photo slide to use image component

--- src/components/PhotoSlide.jsx
+++ src/components/PhotoSlide.jsx
@@ -1,54 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "../css/PhotoSlide.css";
-
-import image1 from "../assets/images/1.jpg";
-import image2 from "../assets/images/2.jpg";
-import image3 from "../assets/images/9.jpeg";
-import image4 from "../assets/images/4.jpg";
-import image5 from "../assets/images/5.jpg";
-import image6 from "../assets/images/6.jpeg";
-import image7 from "../assets/images/7.jpg";
-import image8 from "../assets/images/8.jpg";
-import image9 from "../assets/images/commercial1.jpg";
-import image10 from "../assets/images/commercial2.jpg";
-import image11 from "../assets/images/11.jpg";
-import image12 from "../assets/images/IMG_1589.jpg";
-import image14 from "../assets/images/IMG_1649.jpg";
-import image15 from "../assets/images/bakan1.png";
-import image16 from "../assets/images/bakan2.png";
-import image17 from "../assets/images/9.png";
-import image18 from "../assets/images/10.png";
-import image20 from "../assets/images/12.png";
-import image21 from "../assets/images/13.png";
-import image22 from "../assets/images/14.png";
-import image23 from "../assets/images/15.png";
-import image24 from "../assets/images/16.png";
+import imageData from "./ImageData";
 
 function PhotoSlide() {
-  const [images] = useState([
-    image16,
-    image18,
-    image15,
-    image12,
-    image17,
-    image23,
-    image20,
-    image24,
-    image21,
-    image14,
-    image10,
-    image9,
-    image11,
-    image3,
-    image2,
-    image6,
-    image4,
-    image5,
-    image7,
-    image1,
-    image8,
-    image22,
-  ]);
   const [timer, setTimer] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -72,7 +26,7 @@ function PhotoSlide() {
     setCurrentIndex((prevIndex) => prevIndex - 1);
   };
 
-  const currentImg = images[Math.abs(currentIndex) % images.length];
+  const currentImg = imageData[Math.abs(currentIndex) % imageData.length];
 
   const handleClick = (event) => {
     event.preventDefault(); // Prevent default behavior of anchor tag
