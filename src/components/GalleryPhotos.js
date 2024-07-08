1bb3a767ad0fa06f3bc68193169776e1359a2f38
mwrowan89 <mwrowan89@gmail.com>
2024-07-08 13:44:01 -0600

    Images added to gallery

--- src/components/GalleryPhotos.js
+++ src/components/GalleryPhotos.js
@@ -1,9 +1,52 @@
 import React from "react";
+import image1 from "../assets/images/1.jpg";
+import image2 from "../assets/images/2.jpg";
+import image3 from "../assets/images/9.jpeg";
+import image4 from "../assets/images/4.jpg";
+import image5 from "../assets/images/5.jpg";
+import image6 from "../assets/images/6.jpeg";
+import image7 from "../assets/images/7.jpg";
+import image8 from "../assets/images/8.jpg";
+import image9 from "../assets/images/commercial1.jpg";
+import image10 from "../assets/images/commercial2.jpg";
+import image11 from "../assets/images/11.jpg";
+import image12 from "../assets/images/IMG_1589.jpg";
+import image14 from "../assets/images/IMG_1649.jpg";
+import image15 from "../assets/images/bakan1.png";
+import image16 from "../assets/images/bakan2.png";
+import image17 from "../assets/images/9.png";
+import image18 from "../assets/images/10.png";
+import image20 from "../assets/images/12.png";
+import image21 from "../assets/images/13.png";
+import image22 from "../assets/images/14.png";
+import image23 from "../assets/images/15.png";
+import image24 from "../assets/images/16.png";
 
 const GalleryPhotos = () => {
   return (
     <div>
-      <h1>Photos</h1>
+      <img src={image1} alt="image1" />
+      <img src={image2} alt="image1" />
+      <img src={image3} alt="image1" />
+      <img src={image4} alt="image1" />
+      <img src={image5} alt="image1" />
+      <img src={image6} alt="image1" />
+      <img src={image7} alt="image1" />
+      <img src={image8} alt="image1" />
+      <img src={image9} alt="image1" />
+      <img src={image10} alt="image1" />
+      <img src={image11} alt="image1" />
+      <img src={image12} alt="image1" />
+      <img src={image14} alt="image1" />
+      <img src={image15} alt="image1" />
+      <img src={image16} alt="image1" />
+      <img src={image17} alt="image1" />
+      <img src={image18} alt="image1" />
+      <img src={image20} alt="image1" />
+      <img src={image21} alt="image1" />
+      <img src={image22} alt="image1" />
+      <img src={image23} alt="image1" />
+      <img src={image24} alt="image1" />
     </div>
   );
 };
