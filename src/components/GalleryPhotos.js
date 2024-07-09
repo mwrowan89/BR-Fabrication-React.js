67b777946e33c7e5e0d526d9b9cf6dc999df8c2b
mwrowan89 <mwrowan89@gmail.com>
2024-07-09 11:09:49 -0600

    Working on selection for gallery display

--- src/components/GalleryPhotos.js
+++ src/components/GalleryPhotos.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/GalleryPhotos.css";
 import ImageData from "./ImageData";
 import { useNavigate } from "react-router-dom";
 
 const GalleryPhotos = () => {
+  const [selection, setSelection] = useState("all");
   const navigate = useNavigate();
 
   const handleNavigation = (path, query) => {
