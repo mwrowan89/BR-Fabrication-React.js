fc16d2abaa374b45d5bb1c574bdec87cf07e05de
mwrowan89 <mwrowan89@gmail.com>
2024-07-10 15:56:58 -0600

    Made it so only 10 images show per page in gallery view

--- src/components/GalleryPhotos.js
+++ src/components/GalleryPhotos.js
@@ -5,11 +5,27 @@ import { useNavigate } from "react-router-dom";
 
 const GalleryPhotos = () => {
   const [selection, setSelection] = useState("all");
+  const [page, setPage] = useState(1);
+  const imagesPerPage = 10;
   const navigate = useNavigate();
 
   const handleNavigation = (path, query) => {
     navigate(`${path}?query=${query}`);
   };
+
+  const nextPage = () => {
+    setPage((page) => page + 1);
+  };
+  const prevPage = () => {
+    if (page > 1) {
+      setPage((page) => page - 1);
+    }
+  };
+
+  const startIndex = (page - 1) * imagesPerPage;
+  const endIndex = startIndex + imagesPerPage;
+  const currentImages = ImageData.slice(startIndex, endIndex);
+
   return (
     <div>
       <div className="com-res-btns">
@@ -17,7 +33,7 @@ const GalleryPhotos = () => {
         <h2 onClick={() => setSelection("res")}>Residential Works</h2>
       </div>
       <div className="gallery-images-container">
-        {ImageData.map((image) => (
+        {currentImages.map((image) => (
           <img
             onClick={() => handleNavigation("/details", image.id)}
             id="gallery-image"
@@ -26,6 +42,27 @@ const GalleryPhotos = () => {
             alt={image.desc}
           />
         ))}
+      </div>
+      <div className="next-prev-buttons">
+        <h3
+          id="prev"
+          onClick={(e) => {
+            e.preventDefault();
+            prevPage();
+          }}
+        >
+          Prev Page
+        </h3>
+        <p>{page}</p>
+        <h3
+          id="next"
+          onClick={(e) => {
+            e.preventDefault();
+            nextPage();
+          }}
+        >
+          Next Page
+        </h3>
       </div>
     </div>
   );
