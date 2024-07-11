488c216caf96fc6372fe91eb4035e26f28cc34dd
mwrowan89 <mwrowan89@gmail.com>
2024-07-11 14:30:49 -0600

    Made com and res selection work

--- src/components/GalleryPhotos.js
+++ src/components/GalleryPhotos.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/GalleryPhotos.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import imageData from "./ImageData";
@@ -8,6 +8,7 @@ const GalleryPhotos = () => {
   const queryParams = new URLSearchParams(location.search);
   const initialPage = parseInt(queryParams.get("page")) || 1;
   const initialSelection = queryParams.get("selection") || "all";
+  const [images, setImages] = useState([]);
 
   const [selection, setSelection] = useState(initialSelection);
   const [page, setPage] = useState(initialPage);
@@ -31,24 +32,41 @@ const GalleryPhotos = () => {
     }
   };
 
+  const getImages = (newSelection) => {
+    setSelection(newSelection);
+    if (selection === "all") {
+      setImages(imageData);
+    } else if (selection === "com") {
+      setImages(imageData.filter((image) => image.customer === "com"));
+    } else if (selection === "res") {
+      setImages(imageData.filter((image) => image.customer === "res"));
+    }
+  };
+
+  useEffect(() => {
+    getImages();
+  }, [selection]);
+
   return (
     <div>
       <div className="com-res-btns">
-        <h2 onClick={() => setSelection("com")}>Commercial Works</h2>
-        <h2 onClick={() => setSelection("res")}>Residential Works</h2>
+        <h2 onClick={() => getImages("com")}>Commercial Works</h2>
+        <h2 onClick={() => getImages("res")}>Residential Works</h2>
       </div>
       <div className="gallery-images-container">
-        {imageData.map((image) =>
-          image.page === page ? (
-            <img
-              onClick={() => handleNavigation("/details", image.id)}
-              id="gallery-image"
-              key={image.id}
-              src={image.src}
-              alt={image.desc}
-            />
-          ) : null
-        )}
+        {images
+          .filter((image) => image.page === page)
+          .map((image) =>
+            image.page === page ? (
+              <img
+                onClick={() => handleNavigation("/details", image.id)}
+                id="gallery-image"
+                key={image.id}
+                src={image.src}
+                alt={image.desc}
+              />
+            ) : null
+          )}
       </div>
       <div className="gallery-next-prev-btn">
         <h3
