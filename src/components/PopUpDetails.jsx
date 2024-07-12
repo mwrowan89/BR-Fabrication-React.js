61f6c06d3fb95ca676f6db97bd05fa26a168ead4
mwrowan89 <mwrowan89@gmail.com>
2024-07-12 10:46:18 -0600

    Added logic to popup window comp

--- src/components/PopUpDetails.jsx
+++ src/components/PopUpDetails.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Modal from "react-modal";
 
-const PopUpDetails = () => {
-  return <div>PopUpDetails</div>;
+Modal.setAppElement("#root");
+
+const PopUpDetails = (isOpen, onRequestClose, result) => {
+  return (
+    <div>
+      <Modal
+        className={"pop-up-window"}
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="Result Details"
+      >
+        <div className="pop-up">
+          {result && (
+            <div className="pop-up-results">
+              <img
+                src={
+                  result.poster_path && result.Poster !== "N/A"
+                    ? `https://image.tmdb.org/t/p/w500${result.poster_path}`
+                    : "./NotFound.jpeg"
+                }
+                alt={`${result.Title} poster`}
+              />
+              <h1>{result.title || result.original_name}</h1>
+              <p>{result.overview}</p>
+              <div className="pop-up-results-more-info">
+                <p>Release Date: {result.release_date}</p>
+                <p>
+                  TMDB Viewer Rating: {Math.round(result.vote_average)} / 10
+                </p>
+                <p>Vote Count: {result.vote_count}</p>
+              </div>
+
+              <h2 className="close-x" onClick={onRequestClose}>
+                X
+              </h2>
+            </div>
+          )}
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default PopUpDetails;
