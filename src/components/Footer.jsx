553c9e469641e6f9d68e409b52f1b93dfb555802
mwrowan89 <mwrowan89@gmail.com>
2024-07-14 16:03:14 -0600

    Added contact info in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,9 @@ function Footer() {
   return (
     <div className="footer">
       <p>All Rights Reserved BR Fabrication</p>
+      <h4>Contact Information</h4>
+      <p>Email: ben@fromcheftotable.com</p>
+      <p>PHone: (312) 555-4800 </p>
     </div>
   );
 }
