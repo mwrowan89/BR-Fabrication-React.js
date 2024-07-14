46693cb64e5ca0bb973d1693dbb4bf69d94454a9
mwrowan89 <mwrowan89@gmail.com>
2024-07-14 16:07:17 -0600

    Added links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../css/Footer.css";
+import InstagramLogo from "../assets/images/Screenshot (19).png";
+import FacebookLogo from "../assets/facbook logo11.png";
 
 function Footer() {
   return (
@@ -7,7 +9,30 @@ function Footer() {
       <p>All Rights Reserved BR Fabrication</p>
       <h4>Contact Information</h4>
       <p>Email: ben@fromcheftotable.com</p>
-      <p>Phone: (312) 555-4800 </p>
+      <p>Phone: (312)-555-4800 </p>
+      <div className="links">
+        <a
+          href="https://www.instagram.com/fromcheftotable/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            id="insta"
+            src={InstagramLogo}
+            width="33"
+            height="33"
+            alt="Instagram link"
+          />
+        </a>{" "}
+        &nbsp; || &nbsp;
+        <a
+          href="https://www.facebook.com/fromcheftotable/?locale=it_IT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={FacebookLogo} width="22" height="22" alt="FaceBook Link" />
+        </a>
+      </div>
     </div>
   );
 }
