dbaf28a0c755e3bb33ee3fcfb3ae852fa4405ed1
mwrowan89 <mwrowan89@gmail.com>
2024-07-15 16:26:07 -0600

    Added gallery slide show images

--- src/components/GalleryPhotoSlide.js
+++ src/components/GalleryPhotoSlide.js
@@ -3,54 +3,50 @@ import React from "react";
 import imageData from "./ImageData";
 
 const GalleryPhotoSlide = () => {
-  const [timer, setTimer] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    startSlide();
-    return () => {
-      clearInterval(timer);
-    };
-    // eslint-disable-next-line
-  }, [currentIndex]);
+  // const next = () => {
+  //   setCurrentIndex((prevIndex) => prevIndex + 1);
+  // };
 
-  const startSlide = () => {
-    setTimer(setInterval(next, 8000));
-  };
-
-  const next = () => {
-    setCurrentIndex((prevIndex) => prevIndex + 1);
-  };
-
-  const prev = () => {
-    setCurrentIndex((prevIndex) => prevIndex - 1);
-  };
+  // const prev = () => {
+  //   setCurrentIndex((prevIndex) => prevIndex - 1);
+  // };
 
   const currentImg = imageData[Math.abs(currentIndex) % imageData.length];
 
-  const handleClick = (event) => {
-    event.preventDefault();
-    if (event.target.className === "prev") {
-      prev();
-      console.log(imageData.length);
-    } else if (event.target.className === "next") {
-      next();
-    }
-  };
+  // const handleClick = (event) => {
+  //   event.preventDefault();
+  //   if (event.target.className === "prev") {
+  //     prev();
+  //     console.log(imageData.length);
+  //   } else if (event.target.className === "next") {
+  //     next();
+  //   }
+  // };
 
   return (
     <>
       <div className="photo-slide">
-        <img className="slide-image" src={currentImg.src} alt="" />
+        <div>
+          {imageData.map((image, index) => (
+            <img
+              className="gallery-slide-image"
+              key={index}
+              src={image.src}
+              alt={image.desc}
+            />
+          ))}
+        </div>
 
-        <div className="buttons">
+        {/* <div className="buttons">
           <a className="prev" onClick={handleClick} href="/">
             &#10094; Previous
           </a>
           <a className="next" onClick={handleClick} href="/">
             Next &#10095;
           </a>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GalleryPhotos from "../components/GalleryPhotos";
 import Nav from "../components/Nav";
+import GalleryPhotoSlide from "../components/GalleryPhotoSlide";
 
 function Gallery() {
   return (
     <>
       <Nav />
+      <GalleryPhotoSlide />
       <GalleryPhotos />
     </>
   );
