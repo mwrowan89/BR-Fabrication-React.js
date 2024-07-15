71065456f27d01285d01fae77ab55cad8c98d0e0
mwrowan89 <mwrowan89@gmail.com>
2024-07-15 16:43:08 -0600

    Made logic to shuffle the images in the array

--- src/components/GalleryPhotoSlide.js
+++ src/components/GalleryPhotoSlide.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import imageData from "./ImageData";
 
 const GalleryPhotoSlide = () => {
+  const [shuffledImages, setShuffledImages] = useState([]);
+
+  useEffect(() => {
+    const shuffledArray = (array) => {
+      const shuffled = array.slice();
+      for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      }
+      return shuffled;
+    };
+    setShuffledImages(shuffledArray(imageData));
+  }, []);
   return (
     <>
       <div className="gallery-slide-container">
-        {imageData.map((image, index) => (
+        {shuffledImages.map((image, index) => (
           <img
             className="gallery-slide-image"
             key={index}
