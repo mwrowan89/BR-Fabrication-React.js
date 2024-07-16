834b16e79400abaa6360305d15297a40a8b6f1ed
mwrowan89 <mwrowan89@gmail.com>
2024-07-16 15:08:30 -0600

    Added background image to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "../css/Home.css";
 import Nav from "../components/Nav";
 import PhotoSlide from "../components/PhotoSlide";
+import image1 from "../assets/images/IMG_1780.JPG";
 
 export default function HomePage() {
   return (
     <>
       <Nav />
+      <div className="home-page-background">
+        <img src={image1} alt="w/e" />
+      </div>
       <div className="home-container">
         <PhotoSlide />
       </div>
