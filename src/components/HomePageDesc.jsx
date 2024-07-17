e4ead7da9baf77909d3c7402373d122089392e82
mwrowan89 <mwrowan89@gmail.com>
2024-07-17 15:23:23 -0600

    Home Desc comp updated

--- src/components/HomePageDesc.jsx
+++ src/components/HomePageDesc.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useEffect } from "react";
+import image2 from "../assets/images/IMG_1779.JPG";
+import image3 from "../assets/images/36.jpg";
 
 const HomePageDesc = () => {
-  return <></>;
+  useEffect(() => {
+    const handleScroll = () => {
+      const buttons = document.querySelectorAll(".box-one, .box-two");
+      const scrollPosition = window.scrollY + window.innerHeight;
+
+      buttons.forEach((button) => {
+        if (scrollPosition > button.offsetTop) {
+          button.classList.add("show");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <>
+      <div className="description-box">
+        <div className="box-one">
+          <img id="bakan-desc-img-one" src={image2} alt="Bakan Feature" />
+          <div className="text">
+            <h3>BR Fabrications</h3>
+            <p>
+              Servicing restaurants and clients around the world, BR Fabrication
+              is a professional table manufacture.
+            </p>
+          </div>
+        </div>
+        <div className="box-two">
+          <img id="bakan-desc-img-two" src={image3} alt="Bakan" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePageDesc;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,6 @@ import "../css/Home.css";
 import Nav from "../components/Nav";
 import PhotoSlide from "../components/PhotoSlide";
 import image1 from "../assets/images/IMG_1780.JPG";
-import image2 from "../assets/images/IMG_1779.JPG";
-import image3 from "../assets/images/36.jpg";
 
 export default function HomePage() {
   useEffect(() => {
@@ -37,22 +35,6 @@ export default function HomePage() {
         />
       </div>
       <div className="home-container">
-        <div className="description-box">
-          <div className="box-one">
-            <img id="bakan-desc-img-one" src={image2} alt="Bakan Feature" />
-            <div className="text">
-              <h3>BR Fabrications</h3>
-              <p>
-                Servicing restaurants and clients around the world, BR
-                Fabrication is a professional table manufacture.
-              </p>
-            </div>
-          </div>
-          <div className="box-two">
-            <img id="bakan-desc-img-two" src={image3} alt="Bakan" />
-          </div>
-        </div>
-
         <PhotoSlide />
       </div>
     </>
