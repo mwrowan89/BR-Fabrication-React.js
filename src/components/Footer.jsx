380c48020e1d7237afbb05a12c9607c77ff28a04
mwrowan89 <mwrowan89@gmail.com>
2024-07-18 10:19:28 -0600

    Footer added hours and more info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,33 +6,43 @@ import FacebookLogo from "../assets/facbook logo11.png";
 function Footer() {
   return (
     <div className="footer">
-      <p>All Rights Reserved BR Fabrication</p>
-      <h4>Contact Information</h4>
-      <p>Email: ben@fromcheftotable.com</p>
-      <p>Phone: (312)-555-4800 </p>
+      <div className="footer-left">
+        <p>All Rights Reserved BR Fabrication</p>
+        <h4>Contact Information</h4>
+        <p>Email: ben@fromcheftotable.com</p>
+        <p>Phone: (312)-555-4800 </p>
 
-      <div className="links-footer">
-        <a
-          href="https://www.instagram.com/fromcheftotable/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            id="insta"
-            src={InstagramLogo}
-            width="33"
-            height="33"
-            alt="Instagram link"
-          />
-        </a>{" "}
-        &nbsp; || &nbsp;
-        <a
-          href="https://www.facebook.com/fromcheftotable/?locale=it_IT"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={FacebookLogo} width="22" height="22" alt="FaceBook Link" />
-        </a>
+        <div className="links-footer">
+          <a
+            href="https://www.instagram.com/fromcheftotable/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              id="insta"
+              src={InstagramLogo}
+              width="33"
+              height="33"
+              alt="Instagram link"
+            />
+          </a>{" "}
+          &nbsp; || &nbsp;
+          <a
+            href="https://www.facebook.com/fromcheftotable/?locale=it_IT"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={FacebookLogo}
+              width="22"
+              height="22"
+              alt="FaceBook Link"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="footer-right">
+        <p>Shop Hours: </p>
       </div>
     </div>
   );
