348ca36dfcaac01bf58407eb83a6c39ac1be83c0
mwrowan89 <mwrowan89@gmail.com>
2024-07-31 11:45:47 -0600

    Added design page css and about description

--- src/pages/DesignsPage.jsx
+++ src/pages/DesignsPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import "../css/Designs.css";
 
 const DesignsPage = () => {
   return (
@@ -8,7 +9,19 @@ const DesignsPage = () => {
       <Header />
       <Nav />
       <div className="design-page">
-        <h1>Custom Designs</h1>
+        <h1 id="desgin-page-title">Custom Designs</h1>
+        <p id="design-page-desc">
+          In addition to our woodworking expertise, BR Fabrication also
+          specializes in creating custom computer designs tailored to the
+          specific requirements and preferences of our clients. Whether it’s
+          building high-performance workstations for professional use or
+          personalized gaming setups, we ensure that each design meets the
+          unique specifications of our customers. Our attention to detail and
+          commitment to quality extend to this area, resulting in bespoke
+          systems that blend functionality with aesthetic appeal. This service
+          underscores our dedication to delivering personalized solutions that
+          cater to a wide range of needs and tastes.
+        </p>
       </div>
     </div>
   );
