2ac8bf949561b7a441b39d4f46cfa865b99e6266
mwrowan89 <mwrowan89@gmail.com>
2024-07-31 11:49:13 -0600

    Added stylings to design page along with title and description

--- src/pages/DesignsPage.jsx
+++ src/pages/DesignsPage.jsx
@@ -9,19 +9,21 @@ const DesignsPage = () => {
       <Header />
       <Nav />
       <div className="design-page">
-        <h1 id="desgin-page-title">Custom Designs</h1>
-        <p id="design-page-desc">
-          In addition to our woodworking expertise, BR Fabrication also
-          specializes in creating custom computer designs tailored to the
-          specific requirements and preferences of our clients. Whether it’s
-          building high-performance workstations for professional use or
-          personalized gaming setups, we ensure that each design meets the
-          unique specifications of our customers. Our attention to detail and
-          commitment to quality extend to this area, resulting in bespoke
-          systems that blend functionality with aesthetic appeal. This service
-          underscores our dedication to delivering personalized solutions that
-          cater to a wide range of needs and tastes.
-        </p>
+        <div className="design-box-one">
+          <h1 id="design-page-title">Custom Designs</h1>
+          <p id="design-page-desc">
+            In addition to our woodworking expertise, BR Fabrication also
+            specializes in creating custom computer designs tailored to the
+            specific requirements and preferences of our clients. Whether it’s
+            building high-performance workstations for professional use or
+            personalized gaming setups, we ensure that each design meets the
+            unique specifications of our customers. Our attention to detail and
+            commitment to quality extend to this area, resulting in bespoke
+            systems that blend functionality with aesthetic appeal. This service
+            underscores our dedication to delivering personalized solutions that
+            cater to a wide range of needs and tastes.
+          </p>
+        </div>
       </div>
     </div>
   );
