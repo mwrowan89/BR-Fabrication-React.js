95d160235b233c0e1302ca38af2ca4c4edb0ab78
mwrowan89 <mwrowan89@gmail.com>
2024-08-01 11:23:11 -0600

    Created div for wine cooler

--- src/pages/DesignsPage.jsx
+++ src/pages/DesignsPage.jsx
@@ -33,57 +33,61 @@ const DesignsPage = () => {
       <Header />
       <Nav />
       <div className="design-page">
-        <div className="design-box-one">
-          <h1 id="design-page-title">Custom Designs</h1>
-          <p id="design-page-desc">
-            In addition to our woodworking expertise, BR Fabrication also
-            specializes in creating custom computer designs tailored to the
-            specific requirements and preferences of our clients. Whether it’s
-            building high-performance workstations for professional use or
-            personalized gaming setups, we ensure that each design meets the
-            unique specifications of our customers. Our attention to detail and
-            commitment to quality extend to this area, resulting in bespoke
-            systems that blend functionality with aesthetic appeal. This service
-            underscores our dedication to delivering personalized solutions that
-            cater to a wide range of needs and tastes.
-          </p>
-        </div>
-        <div className="design-box-two">
-          <h1 id="wine-cellar-title">800 Bottle Wine Cooler</h1>
-          <div className="box-two-images-top">
-            <img id="sketch-up-wine" src={image4} alt="wine celler" />
-            <img id="sketch-up-wine" src={image3} alt="wine celler" />
-          </div>
-          <br />
-          <div className="wine-cellar-description">
-            <p>
-              This wine cooler is located in a restaurant in Chicago, IL and
-              features hundreds of bottles of wine for their customers as well
-              as custom lighting. The custom wine cooler features built in
-              cabinetry for the cooling units as well as a solid Oak floor for
-              looks and durability.
+        <div className="wine-cellar">
+          <div className="design-box-one">
+            <h1 id="design-page-title">Custom Designs</h1>
+            <p id="design-page-desc">
+              In addition to our woodworking expertise, BR Fabrication also
+              specializes in creating custom computer designs tailored to the
+              specific requirements and preferences of our clients. Whether it’s
+              building high-performance workstations for professional use or
+              personalized gaming setups, we ensure that each design meets the
+              unique specifications of our customers. Our attention to detail
+              and commitment to quality extend to this area, resulting in
+              bespoke systems that blend functionality with aesthetic appeal.
+              This service underscores our dedication to delivering personalized
+              solutions that cater to a wide range of needs and tastes.
             </p>
           </div>
-          <div className="box-two-images-bottom">
-            <img id="finished-photo" src={image2} alt="wine celler" />
-            <div className="video-container" onClick={handleVideoClick}>
-              <video
-                id="wine-cellar-video"
-                ref={videoRef}
-                loop
-                muted
-                style={{ cursor: "pointer" }}
-              >
-                <source src={video2} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              {!isPlaying && <div className="play-button-overlay">▶️</div>}
+          <div className="design-box-two">
+            <h1 id="wine-cellar-title">800 Bottle Wine Cooler</h1>
+            <div className="box-two-images-top">
+              <img id="sketch-up-wine" src={image4} alt="wine celler" />
+              <img id="sketch-up-wine" src={image3} alt="wine celler" />
             </div>
+            <br />
+            <div className="wine-cellar-description">
+              <p>
+                This wine cooler is located in a restaurant in Chicago, IL and
+                features hundreds of bottles of wine for their customers as well
+                as custom lighting. The custom wine cooler features built in
+                cabinetry for the cooling units as well as a solid Oak floor for
+                looks and durability.
+              </p>
+            </div>
+            <div className="box-two-images-bottom">
+              <img id="finished-photo" src={image2} alt="wine celler" />
+              <div className="video-container" onClick={handleVideoClick}>
+                <video
+                  id="wine-cellar-video"
+                  ref={videoRef}
+                  loop
+                  muted
+                  style={{ cursor: "pointer" }}
+                >
+                  <source src={video2} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+                {!isPlaying && <div className="play-button-overlay">▶️</div>}
+              </div>
 
-            <img id="finished-photo" src={image1} alt="wine celler" />
-            <img id="finished-photo" src={image5} alt="wine celler" />
+              <img id="finished-photo" src={image1} alt="wine celler" />
+              <img id="finished-photo" src={image5} alt="wine celler" />
+            </div>
           </div>
         </div>
+
+        <div className="design-box-three">Something</div>
       </div>
     </div>
   );
