2203ae703263193dfa5085f4d81ab88cd76d7a39
mwrowan89 <mwrowan89@gmail.com>
2024-08-01 10:59:48 -0600

    Added wine cellar descrip

--- src/pages/DesignsPage.jsx
+++ src/pages/DesignsPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef, useEffect } from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import "../css/Designs.css";
@@ -8,9 +9,21 @@ import image2 from "../assets/images/Wine Cellar/IMG_1202.png";
 import image3 from "../assets/images/Wine Cellar/Wine Cellar 1.jpg";
 import image4 from "../assets/images/Wine Cellar/Wine Cellar 2.jpg";
 import image5 from "../assets/images/Wine Cellar/IMG_7321.png";
-import video1 from "../assets/images/Wine Cellar/";
+import video1 from "../assets/images/Wine Cellar/IMG_9383.MOV";
 
 const DesignsPage = () => {
+  const videoRef = useRef(null);
+
+  const handleVideoClick = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
+
   return (
     <div>
       <Header />
@@ -37,8 +50,26 @@ const DesignsPage = () => {
             <img id="sketch-up-wine" src={image4} alt="wine celler" />
             <img id="sketch-up-wine" src={image3} alt="wine celler" />
           </div>
+          <div className="wine-cellar-description">
+            <p>
+              This wine cooler is located in a restaurant in Chicago, Il and
+              features hundreds of bottles of wine for their customers. The
+              custom wine cooler features built in cabinetry for the cooling
+              units as well as a solid Oak floor for looks and durability
+            </p>
+          </div>
           <div className="box-two-images-bottom">
             <img id="finished-photo" src={image2} alt="wine celler" />
+            <video
+              id="wine-cellar-video"
+              ref={videoRef}
+              muted
+              onClick={handleVideoClick}
+              style={{ cursor: "pointer" }}
+            >
+              <source src={video1} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
             <img id="finished-photo" src={image1} alt="wine celler" />
             <img id="finished-photo" src={image5} alt="wine celler" />
           </div>
