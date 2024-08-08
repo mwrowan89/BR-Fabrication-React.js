7d8f71284771f42c44b531c11d4dcc6c4530c4d0
mwrowan89 <mwrowan89@gmail.com>
2024-08-08 08:59:31 -0600

    Updated image data

--- src/components/ImageData.js
+++ src/components/ImageData.js
@@ -1699,9 +1699,9 @@ const imageData = [
   {
     id: 141,
     src: image141,
-    alt: "Golden Acacia Drip",
+    alt: "Golden Acacia Coffee Table",
     desc: "On of our patented 'Drip' tables carved from the Acacia tree to create a flowing transition to the floor. This table is located in a food hall in Salt Lake City, Ut.",
-    title: "Golden Acacia Drip",
+    title: "Golden Acacia Coffee Table",
     customer: "com",
     page: 11,
     type: ["com-table", "drip"],
