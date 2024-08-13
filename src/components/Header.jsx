203fdcde1472e16c32337e6fe328afeeebfb5e1d
mwrowan89 <mwrowan89@gmail.com>
2024-08-13 13:02:53 -0600

    Removed img css from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "../css/Header.css";
-// import InstagramLogo from "../assets/images/Screenshot (19).png";
-// import FacebookLogo from "../assets/facbook logo11.png";
 
 function Header() {
   return (
@@ -11,29 +9,6 @@ function Header() {
         Heirloom Quality Furniture for Commercial & Residential Spaces
       </h3>
       <br />
-      {/* <div className="links">
-        <a
-          href="https://www.instagram.com/fromcheftotable/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            id="insta"
-            src={InstagramLogo}
-            width="33"
-            height="33"
-            alt="Instagram link"
-          />
-        </a>{" "}
-        &nbsp; || &nbsp;
-        <a
-          href="https://www.facebook.com/fromcheftotable/?locale=it_IT"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={FacebookLogo} width="22" height="22" alt="FaceBook Link" />
-        </a>
-      </div> */}
     </div>
   );
 }
