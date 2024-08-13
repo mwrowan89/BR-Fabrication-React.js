1586cc46afa460e75cb656a44628e45c443b3930
mwrowan89 <mwrowan89@gmail.com>
2024-08-13 13:19:41 -0600

    Added request more info button

--- src/components/PopUpDetails.jsx
+++ src/components/PopUpDetails.jsx
@@ -4,6 +4,18 @@ import Modal from "react-modal";
 Modal.setAppElement("#root");
 
 const PopUpDetails = ({ isOpen, onRequestClose, image }) => {
+  const getMoreInfo = (image) => {
+    const subject = encodeURIComponent(
+      `More Information Request: ${image.title}`
+    );
+    const body = encodeURIComponent(
+      `I would like to request more information about the following image:\n\nTitle: ${image.title}\nDescription: ${image.desc}\nImage Source: ${image.id}`
+    );
+
+    const mailtoLink = `mailto:?subject=${subject}&body=${body}`;
+
+    window.location.href = mailtoLink;
+  };
   return (
     <div className="pop-up-conatiner">
       <Modal
@@ -19,6 +31,8 @@ const PopUpDetails = ({ isOpen, onRequestClose, image }) => {
                 <h1 id="pop-up-title">{image.title}</h1>
                 <h5 id="pop-up-image-desc">{image.desc}</h5>
                 <img id="pop-up-image" src={image.src} alt={image.desc} />
+                <br />
+                <h2 onClick={getMoreInfo(image)}>Request More Info</h2>
               </div>
             </div>
           )}
