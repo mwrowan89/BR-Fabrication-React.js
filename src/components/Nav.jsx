cc5902cd13a86e40b9ee57e24362cb83286dfbf1
mwrowan89 <mwrowan89@gmail.com>
2024-08-15 10:20:27 -0600

    Updated nav for responsive design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <>
       <div>
-        <nav>
+        <nav className="nav-bar">
           <ul className="nav-items">
             <li>
               <Link className="nav-link one" to="/">
