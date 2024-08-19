a1c6d7b33fc4e12fd677bed2dd531104b2be64a9
mwrowan89 <mwrowan89@gmail.com>
2024-08-19 08:59:46 -0600

    Updated desgins resize css

--- src/components/GalleryPhotoSlide.js
+++ src/components/GalleryPhotoSlide.js
@@ -13,7 +13,7 @@ const GalleryPhotoSlide = () => {
 
   const updateScrollAmount = () => {
     if (window.innerWidth < 500) {
-      setScrollAmount(250);
+      setScrollAmount(225);
     } else {
       setScrollAmount(1500);
     }
