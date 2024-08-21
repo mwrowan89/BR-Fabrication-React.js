1a16b9f3c6679db84f46f6a3703f2b1d00197391
mwrowan89 <mwrowan89@gmail.com>
2024-08-21 12:06:39 -0600

    Responsive desing updated for contact page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,6 @@
 import Nav from "../components/Nav";
 import ContactForm from "../components/ContactForm";
 import Header from "../components/Header";
-import { useState, useEffect } from "react";
 
 export default function ContactUs() {
   return (
