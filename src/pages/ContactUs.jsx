2fee6e2fd40c0b1fee367dba676dfc1309322c87
mwrowan89 <mwrowan89@gmail.com>
2024-08-21 11:45:32 -0600

    Cleaned up contact page code

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,18 +4,6 @@ import Header from "../components/Header";
 import { useState, useEffect } from "react";
 
 export default function ContactUs() {
-  const [isWindowSize, setIsWindowSize] = useState(false);
-  const windowSize = () => {
-    if (window.innerWidth < 500) {
-      setIsWindowSize(false);
-    } else {
-      setIsWindowSize(true);
-    }
-  };
-  useEffect(() => {
-    window.addEventListener("resize", windowSize);
-    windowSize();
-  }, []);
   return (
     <>
       <Header />
