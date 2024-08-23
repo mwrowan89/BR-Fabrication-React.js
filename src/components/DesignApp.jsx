9f535740b0932aee883cf7abe73a0f92bb3140a8
mwrowan89 <mwrowan89@gmail.com>
2024-08-23 02:45:44 -0600

    updated imports to design comp

--- src/components/DesignApp.jsx
+++ src/components/DesignApp.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { useRef, useState, useEffect } from "react";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import TopBanner from "../components/TopBanner";
+import "../css/Designs.css";
 const DesignApp = () => {
   return <div>DesignApp</div>;
 };
