f9502ffd86153a8d1d27e7791e7a51148aaabb68
mwrowan89 <mwrowan89@gmail.com>
2024-08-23 02:46:50 -0600

    Imported wine cellar photos and videos to design comp

--- src/components/DesignApp.jsx
+++ src/components/DesignApp.jsx
@@ -4,6 +4,17 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import TopBanner from "../components/TopBanner";
 import "../css/Designs.css";
+// Wine Cooler
+import image1 from "../assets/images/Wine Cellar/IMG_1201.png";
+import image2 from "../assets/images/Wine Cellar/IMG_1202.png";
+import image3 from "../assets/images/Wine Cellar/Wine Cellar 1.jpg";
+import image4 from "../assets/images/Wine Cellar/Wine Cellar 2.jpg";
+import image5 from "../assets/images/Wine Cellar/IMG_7321.png";
+import image18 from "../assets/images/Wine Cellar/IMG_6915.jpg";
+import image19 from "../assets/images/Wine Cellar/IMG_6922.jpg";
+import video2 from "../assets/images/Wine Cellar/Cellarvideo2.mov";
+import video5 from "../assets/images/Wine Cellar/IMG_9383.MOV";
+
 const DesignApp = () => {
   return <div>DesignApp</div>;
 };
