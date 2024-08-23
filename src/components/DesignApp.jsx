4fb6aa869347710a24a3aa4c4c7e4d95b675fcb5
mwrowan89 <mwrowan89@gmail.com>
2024-08-23 02:47:27 -0600

    Updated bookcase imports in design comp

--- src/components/DesignApp.jsx
+++ src/components/DesignApp.jsx
@@ -14,7 +14,11 @@ import image18 from "../assets/images/Wine Cellar/IMG_6915.jpg";
 import image19 from "../assets/images/Wine Cellar/IMG_6922.jpg";
 import video2 from "../assets/images/Wine Cellar/Cellarvideo2.mov";
 import video5 from "../assets/images/Wine Cellar/IMG_9383.MOV";
-
+//Walnut Bookcase
+import image6 from "../assets/images/Bookcase/Bookcase 1.jpg";
+import image7 from "../assets/images/Bookcase/Bookcase 2.jpg";
+import image8 from "../assets/images/Bookcase/Bookcase Installed 1.png";
+import image9 from "../assets/images/90.png";
 const DesignApp = () => {
   return <div>DesignApp</div>;
 };
