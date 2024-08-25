e8a011a38463d945c89421677c771fe44f0b3086
mwrowan89 <mwrowan89@gmail.com>
2024-08-25 01:40:15 -0600

    Change design comp imports

--- src/pages/DesignsPage.jsx
+++ src/pages/DesignsPage.jsx
@@ -5,39 +5,6 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import TopBanner from "../components/TopBanner";
 import "../css/Designs.css";
-// Wine Cooler
-import image1 from "../assets/images/Wine Cellar/IMG_1201.png";
-import image2 from "../assets/images/Wine Cellar/IMG_1202.png";
-import image3 from "../assets/images/Wine Cellar/Wine Cellar 1.jpg";
-import image4 from "../assets/images/Wine Cellar/Wine Cellar 2.jpg";
-import image5 from "../assets/images/Wine Cellar/IMG_7321.png";
-import image18 from "../assets/images/Wine Cellar/IMG_6915.jpg";
-import image19 from "../assets/images/Wine Cellar/IMG_6922.jpg";
-import video2 from "../assets/images/Wine Cellar/Cellarvideo2.mov";
-import video5 from "../assets/images/Wine Cellar/IMG_9383.MOV";
-//Walnut Bookcase
-import image6 from "../assets/images/Bookcase/Bookcase 1.jpg";
-import image7 from "../assets/images/Bookcase/Bookcase 2.jpg";
-import image8 from "../assets/images/Bookcase/Bookcase Installed 1.png";
-import image9 from "../assets/images/90.png";
-import image10 from "../assets/images/Bookcase/IMG_2627_Original.png";
-import image20 from "../assets/images/Bookcase/167.png";
-import image21 from "../assets/images/Bookcase/IMG_2631.png";
-import image22 from "../assets/images/Bookcase/IMG_2157.PNG";
-import video3 from "../assets/images/Bookcase/IMG_2654.MOV";
-//Dresser
-import image12 from "../assets/images/Dresser/IMG_6865_Original.png";
-import image13 from "../assets/images/Dresser/IMG_7044_Original.png";
-import image14 from "../assets/images/Dresser/Master Bedroom Chest Brooke Abrams 1.jpg";
-import image16 from "../assets/images/Dresser/Master Bedroom Chest Brooke Abrams-Dimensions.png";
-import image17 from "../assets/images/Dresser/IMG_6785.jpg";
-import video4 from "../assets/images/Dresser/Dresser-vid.mov";
-
-//Top display
-import video6 from "../assets/images/Deskvid.mov";
-import video7 from "../assets/images/Rolling Cabinet/Rolling-cab-vid.mov";
-import image23 from "../assets/images/23-foot-bar.png";
-import image24 from "../assets/images/Oakville.png";
 
 const DesignsPage = () => {
   const wineVideoRef = useRef(null);
@@ -104,6 +71,7 @@ const DesignsPage = () => {
     <>
       <Header />
       <Nav />
+
       <DesignApp />
     </>
   );
