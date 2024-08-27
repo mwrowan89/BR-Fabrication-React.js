8ab16e5fba2cb80403f9ae88f7601f930b163d5d
mwrowan89 <mwrowan89@gmail.com>
2024-08-27 06:36:04 -0600

    Loading comp updated

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import "../css/Loading.css";
 
 const Loading = () => {
-  return (
-    <div>
-      <div id="load">
-        <div>G</div>
-        <div>N</div>
-        <div>I</div>
-        <div>D</div>
-        <div>A</div>
-        <div>O</div>
-        <div>L</div>
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default Loading;
