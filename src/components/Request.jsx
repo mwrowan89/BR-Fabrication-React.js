546f1aa0a0a82f675fabf99c3b2acfbc43c21f17
mwrowan89 <mwrowan89@gmail.com>
2024-08-30 10:58:32 -0600

    Added request header

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Request = () => {
-  return <div>Request</div>;
+  return (
+    <div>
+      <h1>Send Request</h1>
+    </div>
+  );
 };
 
 export default Request;
