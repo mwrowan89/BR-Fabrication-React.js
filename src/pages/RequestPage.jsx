273d0a992ef29b2e8c012318e4468e791575d93f
mwrowan89 <mwrowan89@gmail.com>
2024-08-30 11:01:53 -0600

    Imported nav comp to request page

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Request from "../components/Request";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 const RequestPage = () => {
   return (
     <>
+      <Nav />
       <Header />
       <Request />
     </>
