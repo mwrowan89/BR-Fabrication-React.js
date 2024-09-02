dfb054f53a940f60fbc0cd36a79787bf4fd9a085
mwrowan89 <mwrowan89@gmail.com>
2024-09-02 16:04:07 -0600

    Deleted email logic in request comp

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -20,21 +20,6 @@ const Request = () => {
     console.log(contact);
   };
 
-  const sendEmail = (e, contact) => {
-    e.preventDefault();
-    const subject = encodeURIComponent(
-      `More Information Request from: ${contact.full}`
-    );
-    const body = encodeURIComponent(
-      `I would like to request more information about the following:\n\n ${contact.message}`
-    );
-
-    const mailtoLink = `mailto:?subject=${subject}&body=${body}`;
-
-    setTimeout(() => {
-      window.location.href = mailtoLink;
-    }, 0);
-  };
   return (
     <div>
       <h1>Send Request</h1>
