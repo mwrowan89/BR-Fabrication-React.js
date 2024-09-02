121dd09b5e2be587a0a6de7043194bd47b0fd145
mwrowan89 <mwrowan89@gmail.com>
2024-09-02 16:04:41 -0600

    Request comp updated

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,16 +1,6 @@
 import React from "react";
-import { useState } from "react";
-import FacebookLogo from "../assets/images/facbook logo11.png";
-import InstagramLogo from "../assets/images/Screenshot (19).png";
-import "../css/Request.css";
 
 const Request = () => {
-  const [contact, setContact] = useState({
-    full: "",
-    email: "",
-    message: "Some notes",
-  });
-
   return (
     <div>
       <h1>Send Request</h1>
