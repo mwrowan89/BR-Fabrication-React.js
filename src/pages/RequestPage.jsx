4b575b618ccd5ba925955e7233e4294e31afaece
mwrowan89 <mwrowan89@gmail.com>
2024-09-03 14:59:52 -0600

    Removed imports from requests page

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,16 +1,1 @@
-import React from "react";
-import Request from "../components/Request";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
 
-const RequestPage = () => {
-  return (
-    <>
-      <Nav />
-      <Header />
-      <Request />
-    </>
-  );
-};
-
-export default RequestPage;
